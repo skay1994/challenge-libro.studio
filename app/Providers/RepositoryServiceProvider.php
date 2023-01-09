9d20b5b3d641d955bd4a69bbf79d06e14728a92a
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-09 17:03:50 -0300

    Feat: Add register repository and contract

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -4,14 +4,15 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
-use App\Repositories\Contracts\{CourseRepositoryContract, UserRepositoryContract};
-use App\Repositories\{CourseRepository, UserRepository};
+use App\Repositories\Contracts\{CourseRepositoryContract, RegistryRepositoryContract, UserRepositoryContract};
+use App\Repositories\{CourseRepository, RegistryRepository, UserRepository};
 
 class RepositoryServiceProvider extends ServiceProvider
 {
     private array $repositories = [
         CourseRepositoryContract::class => CourseRepository::class,
-        UserRepositoryContract::class => UserRepository::class
+        UserRepositoryContract::class => UserRepository::class,
+        RegistryRepositoryContract::class => RegistryRepository::class,
     ];
 
     public function register()
