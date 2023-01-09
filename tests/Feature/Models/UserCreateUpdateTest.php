8eabad4ff018c448fc8139f98ad47e42dec5519c
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-09 13:43:44 -0300

    Feat: Create tests for update users

--- tests/Feature/Models/UserCreateUpdateTest.php
+++ tests/Feature/Models/UserCreateUpdateTest.php
@@ -127,3 +127,194 @@ test('Create user fail by invalid birthday', function () {
     $response->assertUnprocessable()
         ->assertJsonValidationErrorFor('birthday_at');
 });
+
+test('Update user', function () {
+    $user = User::factory()->create();
+    $name = fake()->name;
+    $response = $this->putJson(route('users.update', [
+        'user' => $user->getKey()
+    ]), [
+        'name' => $name,
+        'email' => $user->email,
+        'gender' => $user->gender,
+        'birthday_at' => $user->birthday_at,
+    ]);
+    $response->assertOk()
+        ->assertJson([
+            'data' => [
+                'name' => $name
+            ]
+        ]);
+
+    $this->assertDatabaseCount('users', 1);
+    $this->assertDatabaseHas('users', [
+        'name' => $name,
+    ]);
+});
+
+test('Update user with courses', function () {
+    $courses = Course::factory(2)->create();
+    $course = $courses->first();
+
+    $user = User::factory()->create();
+    $name = fake()->name;
+    $response = $this->putJson(route('users.update', [
+        'user' => $user->getKey()
+    ]), [
+        'name' => $name,
+        'email' => fake()->unique()->safeEmail(),
+        'gender' => 'male',
+        'birthday_at' => now()->subYears(20),
+        'course_id' => $courses->pluck('id')
+    ]);
+
+    $response->assertOk()
+        ->assertJson([
+            'data' => [
+                'name' => $name,
+                'courses' => [
+                    [
+                        'id' => $course->getKey(),
+                        'title' => $course->title,
+                    ]
+                ]
+            ]
+        ]);
+
+    $this->assertDatabaseCount('users', 1);
+});
+
+test('Update user with new courses', function () {
+    $oldCourse = Course::factory()->create();
+    $courses = Course::factory(2)->create();
+    $course = $courses->first();
+
+    $user = User::factory()->create();
+    $user->courses()->sync([$oldCourse->getKey()]);
+
+    $name = fake()->name;
+    $response = $this->putJson(route('users.update', [
+        'user' => $user->getKey()
+    ]), [
+        'name' => $name,
+        'email' => fake()->unique()->safeEmail(),
+        'gender' => 'male',
+        'birthday_at' => now()->subYears(20),
+        'course_id' => $courses->pluck('id')
+    ]);
+
+    $response->assertOk()
+        ->assertJson([
+            'data' => [
+                'name' => $name,
+                'courses' => [
+                    [
+                        'id' => $course->getKey(),
+                        'title' => $course->title,
+                    ]
+                ]
+            ]
+        ]);
+
+    $this->assertDatabaseCount('users', 1);
+    $this->assertDatabaseMissing('course_user', [
+        'user_id' => $user->getKey(),
+        'course_id' => $oldCourse->getKey()
+    ]);
+});
+
+test('Update user fail by invalid course', function () {
+    $user = User::factory()->create();
+    $response = $this->putJson(route('users.update', [
+        'user' => $user->getKey()
+    ]), [
+        'name' => fake()->name,
+        'email' => fake()->unique()->safeEmail(),
+        'gender' => 'male',
+        'birthday_at' => now()->subYears(20),
+        'course_id' => ['99']
+    ]);
+    $response->assertUnprocessable()
+        ->assertJsonValidationErrorFor('course_id.0');
+});
+
+test('Update user fail by missing name', function () {
+    $user = User::factory()->create();
+    $response = $this->putJson(route('users.update', [
+        'user' => $user->getKey()
+    ]), [
+        'email' => fake()->unique()->safeEmail(),
+        'gender' => 'male',
+        'birthday_at' => now()->subYears(20),
+    ]);
+    $response->assertUnprocessable()
+        ->assertJsonValidationErrorFor('name');
+});
+
+test('Update user fail by invalid email', function () {
+    $user = User::factory()->create();
+    $response = $this->putJson(route('users.update', [
+        'user' => $user->getKey()
+    ]), [
+        'name' => fake()->name,
+        'email' => 'sometexthere',
+        'gender' => 'male',
+        'birthday_at' => now()->subYears(20),
+    ]);
+    $response->assertUnprocessable()
+        ->assertJsonValidationErrorFor('email');
+});
+
+test('Update user fail by missing email', function () {
+    $user = User::factory()->create();
+    $response = $this->putJson(route('users.update', [
+        'user' => $user->getKey()
+    ]), [
+        'name' => fake()->name,
+        'gender' => 'male',
+        'birthday_at' => now()->subYears(20),
+    ]);
+    $response->assertUnprocessable()
+        ->assertJsonValidationErrorFor('email');
+});
+
+test('Update user fail by invalid gender', function () {
+    $user = User::factory()->create();
+    $response = $this->putJson(route('users.update', [
+        'user' => $user->getKey()
+    ]), [
+        'name' => fake()->name,
+        'email' => fake()->unique()->safeEmail(),
+        'gender' => 'othergender',
+        'birthday_at' => now()->subYears(20),
+    ]);
+    $response->assertUnprocessable()
+        ->assertJsonValidationErrorFor('gender');
+});
+
+test('Update user fail by missing birthday', function () {
+    $user = User::factory()->create();
+    $response = $this->putJson(route('users.update', [
+        'user' => $user->getKey()
+    ]), [
+        'name' => fake()->name,
+        'email' => fake()->unique()->safeEmail(),
+        'gender' => 'male',
+    ]);
+    $response->assertUnprocessable()
+        ->assertJsonValidationErrorFor('birthday_at');
+});
+
+test('Update user fail by invalid birthday', function () {
+    $user = User::factory()->create();
+    $response = $this->putJson(route('users.update', [
+        'user' => $user->getKey()
+    ]), [
+        'name' => fake()->name,
+        'email' => fake()->unique()->safeEmail(),
+        'gender' => 'male',
+        'birthday_at' => 'birthday',
+    ]);
+    $response->assertUnprocessable()
+        ->assertJsonValidationErrorFor('birthday_at');
+});
