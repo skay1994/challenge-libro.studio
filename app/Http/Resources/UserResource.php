a2f669ce87a2933b71bcb651f2296584e5610d1c
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-09 13:04:02 -0300

    Feat: Create UserResource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin User
+ */
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->getKey(),
+            'name' => $this->name,
+            'email' => $this->email,
+            'birthday_at' => $this->birthday_at,
+            'courses' => $this->whenLoaded('courses', function () {
+                return $this->courses->map->only(['id', 'title']);
+            })
+        ];
+    }
+}
