fad85262863d699917876a53c7b498129a3e33ea
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-09 11:25:24 -0300

    Feat: Add User repository and contract

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -4,13 +4,14 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
-use App\Repositories\Contracts\{CourseRepositoryContract};
-use App\Repositories\{CourseRepository};
+use App\Repositories\Contracts\{CourseRepositoryContract, UserRepositoryContract};
+use App\Repositories\{CourseRepository, UserRepository};
 
 class RepositoryServiceProvider extends ServiceProvider
 {
     private array $repositories = [
         CourseRepositoryContract::class => CourseRepository::class,
+        UserRepositoryContract::class => UserRepository::class
     ];
 
     public function register()
