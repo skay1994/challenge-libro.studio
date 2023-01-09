eaa3b095fdc5dd7ff6df03050ed1dd7ba0c388c3
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-09 11:24:14 -0300

    Feat: Update migration with new fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('gender', ['male', 'female'])->index();
+            $table->date('birthday_at')->index();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
